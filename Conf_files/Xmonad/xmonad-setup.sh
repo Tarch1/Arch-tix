ea4bff4a14a63f8dc7380d71f972568b5c3c65fd
Tarch1 <menegonitobia@gmail.com>
2021-02-23 17:53:57 +0100

    Create xmonad-setup.sh

---
+++ Conf_files/Xmonad/xmonad-setup.sh
@@ -0,0 +1,4 @@
+sudo pacman -S xmonad xmonad-contrib xmobar --noconfirm
+mkdir ~/.xmonad
+cp ~/Arch-tix/Conf_files/Xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+cp ~/Arch-tix/Conf_files/Xmonad/xmobarrc ~/.xmobarrc
