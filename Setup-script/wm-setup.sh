f6def7ee0a923e22156ad3db389da630df23765f
Tarch1 <menegonitobia@gmail.com>
2021-02-24 13:02:32 +0100

    Update wm-setup.sh

--- Setup-script/wm-setup.sh
+++ Setup-script/wm-setup.sh
@@ -1,7 +1,7 @@
-chmod +x ~/Arch-tix/Conf_files/Bspwm/bspwm-setup.sh
-chmod +x ~/Arch-tix/Conf_files/Herbstluftwm/herbstluftwm-setup.sh
-chmod +x ~/Arch-tix/Conf_files/Xmonad/xmonad-setup.sh
-chmod +x ~/Arch-tix/Conf_files/Polybar/polybar-setup.sh
+sudo chmod +x ~/Arch-tix/Conf_files/Bspwm/bspwm-setup.sh
+sudo chmod +x ~/Arch-tix/Conf_files/Herbstluftwm/herbstluftwm-setup.sh
+sudo chmod +x ~/Arch-tix/Conf_files/Xmonad/xmonad-setup.sh
+sudo chmod +x ~/Arch-tix/Conf_files/Polybar/polybar-setup.sh
 
 ~/Arch-tix/Conf_files/Herbstluftwm/herbstluftwm-setup.sh
 ~/./Arch-tix/Conf_files/Xmonad/xmonad-setup.sh
