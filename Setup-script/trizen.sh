c5585a7aea06d16b3110d74c419c22b862d6d38e
Tarch1 <menegonitobia@gmail.com>
2021-02-24 12:37:26 +0100

    Update trizen.sh

--- Setup-script/trizen.sh
+++ Setup-script/trizen.sh
@@ -2,5 +2,5 @@ git clone https://aur.archlinux.org/trizen.git ~/trizen
 cd ~/trizen
 makepkg -si
 
-trizen -S picom-ibhagwan-git font-manager bibata-cursor-translucent
-# gllock-git xidlehook wpgtk-git ttf-font-awesome betterlockscreen bibata-rainbow-cursor-theme
+trizen -S picom-ibhagwan-git bibata-cursor-translucent
+# gllock-git xidlehook wpgtk-git font-manager ttf-font-awesome betterlockscreen bibata-rainbow-cursor-theme
