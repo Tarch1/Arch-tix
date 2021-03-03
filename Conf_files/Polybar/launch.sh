d1ff2739665117fcc2358fd8702b98c8a3fd2a7d
Tarch1 <menegonitobia@gmail.com>
2021-03-03 12:02:44 +0100

    Update launch.sh

--- Conf_files/Polybar/launch.sh
+++ Conf_files/Polybar/launch.sh
@@ -7,4 +7,5 @@ export color0_alpha="#22${color1/'#'}"
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 #echo "Bars launched..."
 sleep 1
-~/.config/polybar/hide -w -d top -N 'polybar' --region 560x10+800+-100 & disown
+#~/.config/polybar/hide -w -d top -N 'polybar' --region 560x10+800+-100 & disown
+~/.config/polybar/hide -w -d top -N 'polybar' -d top -H -p 3 & disown
