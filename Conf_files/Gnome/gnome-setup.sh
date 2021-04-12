effc690ae9e4e34e88382a4b8913472e7c2eda4b
Tarch1 <menegonitobia@gmail.com>
2021-04-12 17:59:08 +0200

    Update gnome-setup.sh

--- Conf_files/Gnome/gnome-setup.sh
+++ Conf_files/Gnome/gnome-setup.sh
@@ -1,1 +1,1 @@
-sudo pacman -Syu gnome-shell chrome-gnome-shell gnome-control-center gdm gdm-openrc gnome-weather gnuchess gnome-chess --noconfirm
+sudo pacman -Syu gnome-shell chrome-gnome-shell gnome-control-center gdm gdm-openrc gnome-weather gnuchess gnome-chess gnome-calculator --noconfirm
