6a7649f8bff2493ba1183f7f42294c1bd6e6fa17
Tarch1 <menegonitobia@gmail.com>
2021-04-12 12:28:28 +0200

    Update dm-setup.sh

--- Setup-script/dm-setup.sh
+++ Setup-script/dm-setup.sh
@@ -1,4 +1,4 @@
-sudo pacman -Sy gdm gdm-openrc --noconfirm #lightdm-webkit2-greeter lightdm-webkit-theme-litarvan lightdm-openrc --noconfirm
-#sudo sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter/' /etc/lightdm/lightdm.conf
-#sudo sed -i 's/antergos/litarvan/g' /etc/lightdm/lightdm-webkit2-greeter.conf
-sudo rc-update add gdm default
+sudo pacman -Sy lightdm-webkit2-greeter lightdm-webkit-theme-litarvan lightdm-openrc --noconfirm
+sudo sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter/' /etc/lightdm/lightdm.conf
+sudo sed -i 's/antergos/litarvan/g' /etc/lightdm/lightdm-webkit2-greeter.conf
+sudo rc-update add lightdm default
