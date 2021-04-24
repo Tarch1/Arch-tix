28a1a7b0c2d0d675d6dedbdee9054d02ac9286b8
Tarch1 <menegonitobia@gmail.com>
2021-04-24 13:12:01 +0200

    Update gnome-setup.sh

--- Conf_files/Gnome/gnome-setup.sh
+++ Conf_files/Gnome/gnome-setup.sh
@@ -1,1 +1,5 @@
 sudo pacman -Syu gnome-shell chrome-gnome-shell gnome-control-center gdm gdm-openrc gnome-weather gnuchess gnome-chess gnome-calculator gnome-keyring evince --noconfirm
+mkdir -p /etc/xdg/autostart/
+cp ~/Arch-tix/Conf_files/Gnome/pipewire.desktop /etc/xdg/autostart/
+cp ~/Arch-tix/Conf_files/Gnome/pipewire.sh ~/.config/
+chmod +x ~/.config/pipewire.sh
